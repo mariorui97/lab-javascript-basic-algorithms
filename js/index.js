473fbe467eb59eb4b7549b62106277e42b10a45d
Mário Rui <marinhocosta@live.com.pt>
2021-10-18 17:51:44 +0100

    lab sol. iter. 3

--- js/index.js
+++ js/index.js
@@ -15,3 +15,39 @@ if (hacker1.length > hacker2.length){
 }
 
 // Iteration 3: Loops
+let newName = ""
+
+for (let i = 0; i < hacker1.length; i++){
+    newName = newName + hacker1[i].toUpperCase() + " "   
+}
+// --------------------------------------------------------
+let nameBackwards = ""
+
+for (let b = hacker1.length -1; b >= 0; b--){
+    nameBackwards = nameBackwards + hacker1[b]
+}
+
+// --------------------------------------------------------
+
+let nameOrdered1 = ""
+let nameOrdered2 = ""
+
+for (let x = 0; x < hacker1.length; x++){
+    nameOrdered1 = nameOrdered1 + hacker1[x]
+    nameOrdered2 = nameOrdered2 + hacker2[x]
+          
+    if(nameOrdered1.charAt(x) < nameOrdered2.charAt(x)){
+        console.log("The driver's name goes first.")
+        break;
+    } else if(nameOrdered1.charAt(x) > nameOrdered2.charAt(x)){
+        console.log("Yo, the navigator goes first definitely.")
+        break;
+    }     
+}
+
+if(hacker1.length == hacker2.length){
+  console.log("What?! You both have the same name?")
+};
+
+
+
