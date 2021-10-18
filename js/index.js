f8abae7a07b358af8d9f22498be7f0c9a73fb347
Mário Rui <marinhocosta@live.com.pt>
2021-10-18 15:35:28 +0100

    lab sol. iter. 2

--- js/index.js
+++ js/index.js
@@ -4,7 +4,14 @@ console.log(`The driver's name is ${hacker1}`);
 
 let hacker2 = "Rui"
 console.log("The navigator's name is " +hacker2)
-// Iteration 2: Conditionals
 
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length){
+    console.log("The driver has the longest name, it has " +hacker1.length+ " characters.")     
+} else if (hacker1.length < hacker2.length) {
+    console.log("It seems that the navigator has the longest name, it has " +hacker2.length+ " characters.")
+} else if (hacker1.length == hacker2.length) {
+    console.log("Wow, you both have equally long names, " +hacker2.length+ " characters!")
+}
 
 // Iteration 3: Loops
