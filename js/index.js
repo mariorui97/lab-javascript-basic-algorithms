ad42ddad32e5784747769bc9d06aee7c1e921216
Mário Rui <marinhocosta@live.com.pt>
2021-10-18 15:14:11 +0100

    lab sol. iter. 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // Iteration 1: Names and Input
-// 
+let hacker1 = "Mario"
+console.log(`The driver's name is ${hacker1}`);
 
+let hacker2 = "Rui"
+console.log("The navigator's name is " +hacker2)
 // Iteration 2: Conditionals
 
 
